6fee7ec38da1d771b823f833b8c69bb9aac22acd
Ryan Chandler <ryangjchandler@gmail.com>
2024-10-01 16:08:40 +0100

    wip

--- src/Models/Meta.php
+++ src/Models/Meta.php
@@ -2,6 +2,7 @@
 
 namespace C6Digital\OgManager\Models;
 
+use Closure;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Facades\Storage;
@@ -18,15 +19,26 @@ class Meta extends Model
 
     protected $guarded = [];
 
+    protected static ?Closure $generateImageUrlUsing = null;
+
     public function metable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public static function generateImageUrlUsing(Closure $callback): void
+    {
+        static::$generateImageUrlUsing = $callback;
     }
 
     public function getImageUrl(): ?string
     {
         if (! $this->image) {
             return null;
+        }
+
+        if (isset(static::$generateImageUrlUsing)) {
+            return call_user_func(static::$generateImageUrlUsing, $this->image);
         }
 
         return Storage::url($this->image);
